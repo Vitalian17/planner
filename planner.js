02500357c2dc5285282089a68e4bf99f14269c34
Tony Sarni <sarni.tony@gmail.com>
2019-11-15 18:05:23 -0600

    updated html, css, and js to half way done

---
+++ planner.js
@@ -0,0 +1,5 @@
+var workHours = ['9AM' ,'10AM' ,'11AM' ,'12PM' ,'1PM' ,'2PM' ,'3PM' ,'4PM' ,'5PM' ];
+workHours.forEach(function(item){
+    $('.container').append('<hr>');
+    $('.container').append('<div class="row">' + `<div class="col-md-2">${item}</div>` + '<input class="col-md-9">' + '<div type="button" class="col-md-1">🔒</div>' +'</div>');
+});
