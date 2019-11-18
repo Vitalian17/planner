7daa2ed013e2ca9d91ef89f2302564583bf0bd81
Tony Sarni <sarni.tony@gmail.com>
2019-11-17 22:50:43 -0600

    got the moment.js clock working

--- planner.js
+++ planner.js
@@ -1,5 +1,17 @@
 var workHours = ['9AM' ,'10AM' ,'11AM' ,'12PM' ,'1PM' ,'2PM' ,'3PM' ,'4PM' ,'5PM' ];
 workHours.forEach(function(item){
     $('.container').append('<hr>');
-    $('.container').append('<div class="row">' + `<div class="col-md-2">${item}</div>` + '<input class="col-md-9">' + '<div type="button" class="col-md-1">🔒</div>' +'</div>');
+    $('.container').append('<div class="row">' + `<div class="col-md-2">${item}</div>` + '<input class="col-md-9">' + '<div type="button" class="col-md-1" id="lockPic">🔒</div>' +'</div>');
 });
+
+const clock = document.getElementById('clock');
+
+function updateTime() {
+    const now = moment();
+    const humanReadable = now.format("dddd, MMMM Do YYYY, hh:mma");
+    clock.textContent = humanReadable;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+    
